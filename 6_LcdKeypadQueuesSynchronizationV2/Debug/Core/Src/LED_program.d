eb3d995542fe3d2faa2b90ba6ff8881de7e03ec5
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-07 09:01:20 +0300

    Add files via upload

---
+++ 6_LcdKeypadQueuesSynchronizationV2/Debug/Core/Src/LED_program.d
@@ -0,0 +1,15 @@
+Core/Src/LED_program.o: ../Core/Src/LED_program.c ../Core/Inc/STD_TYPES.h \
+ ../Core/Inc/BIT_MATH.h ../Core/Inc/LED_interface.h \
+ ../Core/Inc/BIT_MATH.h ../Core/Inc/STD_TYPES.h ../Core/Inc/LED_config.h \
+ ../Core/Inc/GPIO_interface.h ../Core/Inc/GPIO_private.h \
+ ../Core/Inc/GPIO_config.h ../Core/Inc/LED_config.h
+../Core/Inc/STD_TYPES.h:
+../Core/Inc/BIT_MATH.h:
+../Core/Inc/LED_interface.h:
+../Core/Inc/BIT_MATH.h:
+../Core/Inc/STD_TYPES.h:
+../Core/Inc/LED_config.h:
+../Core/Inc/GPIO_interface.h:
+../Core/Inc/GPIO_private.h:
+../Core/Inc/GPIO_config.h:
+../Core/Inc/LED_config.h:
